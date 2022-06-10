a2aaeb0bab7233ef76da47ee6083dfe17e37771b
Angela Yang <94413896+angelayknz@users.noreply.github.com>
2022-06-11 08:49:39 +1200

    server.post()

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 
 const server = express()
 
+const shortid = require('shortid')
+
 const PORT = 3000
 
 server.listen(PORT, () => {
   console.log(`server running on localhost: ${PORT}`)
 })
+
+server.use(express.json())
+
+// server.get() **************************
 
 server.get('/', (req, res) => {
   res.json({ hello: 'world' })
@@ -16,10 +22,33 @@ server.get('/hello', (req, res) => {
   res.json({ hello: 'hi' })
 })
 
+server.get('/api/channels', (req, res) => {
+  res.json({ hello: '/api/channels' })
+})
+
+// server.post() **************************
+
+const lessons = [{ name: 'mod1', id: '1', cohort: 'sdffg' }]
+
+// const channels = [
+//   { name: 'mod1', id: '1', cohort: 'sdffg' },
+//   { name: 'mod2', id: '2', cohort: 'sdffg' },
+// ]
+
+const channels = []
+
+server.post('/api/channels', (req, res) => {
+  // const channelInfo = req.body //object
+  const channelInfo = { name: 'mod1', id: '1', cohort: 'sdffg' }
+  // channelInfo.id = shortid.generate()
+  channels.push(channelInfo)
+  res.status(201).json(channelInfo)
+})
+
 server.get('/api/channels/:id', (req, res) => {
   // const viewData = { name: 'mod1', lessonId: '1', cohort: 'sdffg' }
   // res.json(viewData)
-  const lessons = [{ name: 'mod1', id: '1', cohort: 'sdffg' }]
+  // const lessons = [{ name: 'mod1', id: '1', cohort: 'sdffg' }]
   const { id } = req.params
   const found = lessons.find((lesson) => lesson.id == id)
 
@@ -36,10 +65,10 @@ server.put('/api/channels/:id', (req, res) => {
 
   const index = channels.findIndex((channel) => channel.id == id)
 
-  const channels = [
-    { name: 'mod1', id: '1', cohort: 'sdffg' },
-    { name: 'mod2', id: '2', cohort: 'sdffg' },
-  ]
+  // const channels = [
+  //   { name: 'mod1', id: '1', cohort: 'sdffg' },
+  //   { name: 'mod2', id: '2', cohort: 'sdffg' },
+  // ]
 
   if (index != -1) {
     channels[index] = changes
